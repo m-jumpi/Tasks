f9e1db02654e0c37dc117db9f82eeb3477856e42
denis <jumpibox@gmail.com>
2021-11-22 02:58:24 +0300

    task04

---
+++ task04/task04-01/task04-01/Operation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Operation
+{
+    class Operation
+    {
+        public static double Square(double a, double b, double c)
+        {
+            if (TrianglePresence(a, b, c))
+            {
+                double p = a + b + c;
+                double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return s;
+            }
+            else
+            {
+                Console.WriteLine("Cannot computer Square");
+                return 0;
+            }
+
+        }
+
+        public static double Square(double a)
+        {
+            if (TrianglePresence(a))
+            {
+                double p = 3 * a;
+                double s = Math.Sqrt(p * (p - a) * (p - a) * (p - a));
+                return s;
+            }
+            else
+            {
+                Console.WriteLine("Cannot computer Square");
+                return 0;
+            }
+
+        }
+
+        private static bool TrianglePresence(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool TrianglePresence(double a)
+        {
+            if (a > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ task04/task04-01/task04-01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Operation
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please select a type of triangle:\n(0) - To quit\n(1) - Equilateral\n(2) - Regular");
+            int choice = ChoiceCheck();
+
+            if (choice == 1)
+            {
+                Console.WriteLine("Please enter one side of triangle: 'a'");
+                double a = IntCheck("Enter value 'a'");
+                Console.WriteLine($"Square = {(Operation.Square(a)):F2}");
+            }
+            if (choice == 2)
+            {
+                Console.WriteLine("Please enter sides of triangle: 'a', 'b' and 'c'");
+
+                double a = IntCheck("Enter value 'a'");
+                double b = IntCheck("Enter value 'b'");
+                double c = IntCheck("Enter value 'c'");
+                Console.WriteLine($"Square = {(Operation.Square(a, b, c)):F2}");
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        public static int ChoiceCheck()
+        {
+            var inputLine = Console.ReadLine();
+            int result;
+            while (!int.TryParse(inputLine, out result) || !(result == 1 || result == 2 || result == 0))
+            {
+                Console.WriteLine("Invalid Input");
+                inputLine = Console.ReadLine();
+            }
+            return result;
+        }
+
+        public static double IntCheck(string message)
+        {
+            Console.WriteLine(message);
+            var inputLine = Console.ReadLine();
+            double resutl;
+            while (!double.TryParse(inputLine, out resutl))
+            {
+                Console.WriteLine("Invalid Input");
+                inputLine = Console.ReadLine();
+            }
+            return resutl;
+        }
+    }
+}
