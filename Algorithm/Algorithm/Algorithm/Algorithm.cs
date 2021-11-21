08d1e75f64d802ea31f486e0c232c8f71bd3c932
denis <jumpibox@gmail.com>
2021-11-22 02:41:11 +0300

    Algorithm

---
+++ Algorithm/Algorithm/Algorithm/Algorithm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double target = 2023;
+        double x = Sqrt(target);
+        Console.WriteLine(x);
+        Console.WriteLine(x * x);
+    }
+
+    public static double Sqrt(double target)
+    {
+        double x = 1;
+        double oldx;
+        do
+        {
+            oldx = x;
+            x = (x + target / x) / 2;
+        }
+        while (oldx != x);
+        return x;
+    }
+}
