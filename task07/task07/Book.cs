811e02666f69f55e19195d1bf211e74b579ed036
denis <jumpibox@gmail.com>
2021-12-02 15:31:34 +0300

    task07

---
+++ task07/task07/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Book
+{
+    class Book : IComparable
+    {
+        public string author { get; private set; }
+        public string title { get; private set; }
+        public int year { get; private set; }
+        public int pages { get; private set; }
+
+        public void SetBook(string author, string title, int year, int pages)
+        {
+            this.author = author;
+            this.title = title;
+            this.year = year;
+            this.pages = pages;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\nBook:\n Author: {0}\n Title: {1}\n Year: {2}\n {3} pages.\n", author, title, year, pages);
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            Book it = (Book)obj;
+            if (this.year == it.year) return 0;
+            else if (this.year > it.year) return 1;
+            else return -1;
+        }
+    }
+}
---
+++ task07/task07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Book
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Book book1 = new Book();
+            book1.SetBook("Author1", "Title1", 1949, 100);
+
+            Book book2 = new Book();
+            book2.SetBook("Author2", "Title2", 1936, 50);
+
+            Book book3 = new Book();
+            book3.SetBook("Author3", "Title3", 1988, 200);
+
+            Book book4 = new Book();
+            book4.SetBook("Author4", "Title4", 1988, 150);
+
+            Book book5 = new Book();
+            book5.SetBook("Author5", "Title5", 1998, 100);
+
+            Book[] arrayBooks = new Book[] { book1, book2, book3, book4, book5 };
+
+            Array.Sort(arrayBooks);
+
+            foreach (var book in arrayBooks)
+            {
+                book.Show();
+            }
+        }
+    }
+}
+
