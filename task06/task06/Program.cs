5f9e26451bc9ec6d7c28721935d4099ba94cacba
denis <jumpibox@gmail.com>
2021-12-02 15:18:28 +0300

    task06

---
+++ task06/task06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triangel
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Triangel tr = new Triangel();
+                tr.Populate(4, 2, 5);
+                double perimeter = tr.Perimeter();
+                double square = tr.Square();
+                Console.WriteLine(tr);
+                Console.WriteLine($"Square = {square:N2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ task06/task06/Triangel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Triangel
+{
+    public class Triangel
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        public void Populate(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+            else
+            {
+                throw new ArgumentException("Side of tirangle cannot be less than or equal to zero!");
+            }
+        }
+
+        public double Perimeter() => A + B + C;
+
+        public double Square()
+        {
+            double p = Perimeter() / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public override string ToString() => $"Side 'a' = {A},\nSide 'b' = {B},\nSide 'c' = {C}";
+    }
+}
