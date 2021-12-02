f13c9d84b7f8c554794d6da6fdfd0b16dcb9970e
denis <jumpibox@gmail.com>
2021-12-02 14:18:18 +0300

    task04-02

---
+++ task04/task04-02/task04-02/task04-02/Equation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Equation
+{
+    public class Equation
+    {
+        public static int SolutionEquation(int a, int b, int c, out double x1, out double x2)
+        {
+            double d = Math.Pow(b, 2) - 4 * a * c;
+
+            if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / 2 * a;
+                x2 = (-b - Math.Sqrt(d)) / 2 * a;
+                return 1;
+            }
+            if (d == 0)
+            {
+                x1 = x2 = -b / 2 * a;
+                return 0;
+            }
+            else
+            {
+                x1 = x2 = 0;
+                return -1;
+            }
+        }
+    }
+}
---
+++ task04/task04-02/task04-02/task04-02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Equation
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter coefficients 'a', 'b' and 'c' for Quadratic Equation");
+            int a = IntCheck("Enter value 'a':");
+            int b = IntCheck("Enter value 'b':");
+            int c = IntCheck("Enter value 'c':");
+
+            int result = Equation.SolutionEquation(a, b, c, out double x1, out double x2);
+
+            switch (result)
+            {
+                case 1:
+                    Console.WriteLine($"There are two roots with coefficients a={a}, b={b}, c={c} : x1={x1}, x2={x2}.");
+                    break;
+                case 0:
+                    Console.WriteLine($"There is one root with coefficients a={a}, b={b}, c={c} : x1=x2={x1}.");
+                    break;
+                default:
+                    Console.WriteLine($"There are no roots with coefficients a={a}, b={b}, c={c} : no result.");
+                    break;
+            }
+        }
+
+        public static int IntCheck(string message)
+        {
+            Console.WriteLine(message);
+            var inputLine = Console.ReadLine();
+            int resutl;
+            while (!int.TryParse(inputLine, out resutl))
+            {
+                Console.WriteLine("Invalid Input");
+                inputLine = Console.ReadLine();
+            }
+            return resutl;
+        }
+    }
+}
