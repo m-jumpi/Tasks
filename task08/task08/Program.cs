ac04713c6c7afb09f43b19555a70f10a20375990
denis <jumpibox@gmail.com>
2021-12-03 16:15:57 +0300

    task08

---
+++ task08/task08/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Triangel
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Triangel tr = new Triangel(4);
+                double perimeter = tr.Perimeter();
+                double square = tr.Square();
+                Console.WriteLine(tr);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ task08/task08/Triangel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Triangel
+{
+    public class Triangel
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+        private bool exists = false;
+
+        public Triangel(double a, double b, double c)
+        {
+            if (Exist(a, b, c))
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+        }
+
+        public Triangel(double a) : this(a, a, a)
+        {
+        }
+
+        public void Populate(double a, double b, double c)
+        {
+            if (Exist(a, b, c))
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+        }
+
+        public double Perimeter() => A + B + C;
+
+        public double Square()
+        {
+            double p = Perimeter() / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public override string ToString() => $"Side 'a' = {A},\nSide 'b' = {B},\nSide 'c' = {C}";
+
+        private bool Exist(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                A = a;
+                B = b;
+                C = c;
+                return exists = true;
+            }
+            else
+            {
+                throw new ArgumentException("Side of tirangle cannot be less than or equal to zero!");
+            }
+        }
+    }
+}
